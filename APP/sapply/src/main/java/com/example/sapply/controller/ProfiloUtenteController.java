f802f2e66707dd0aaf0c25e3e03f3a3da87f69f7
CesareTuc <cesare.tucci01@gmail.com>
2024-10-25 11:05:05 +0200

    Profilo Utente Controller e html
    Unico problema: data di nascita dà errore perchè il database non mi permette di mettere date inferiori al 2014.

--- APP/sapply/src/main/java/com/example/sapply/controller/ProfiloUtenteController.java
+++ APP/sapply/src/main/java/com/example/sapply/controller/ProfiloUtenteController.java
@@ -1,6 +1,8 @@
 package com.example.sapply.controller;
 
+import com.example.sapply.model.Utente;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/profiloutente")
 public class ProfiloUtenteController {
 
+
     @GetMapping
     public String getPage(Model model, HttpSession session){
-        return "profilo-utente";
+        Utente utente = (Utente) session.getAttribute("utente");
+        // dovrebbe essere già presente dalla fase di Login/Registrazione
 
+        if(utente != null){ // se l'utente è effettivamente loggato in sessione
+            model.addAttribute("utente", utente);
+            // aggiungi al model l'utente (ricavato da session)
+            // questo serve perchè altrimenti la pagina .html non sa da dove prendere le info dell'utente
+            return "profilo-utente";
+        }
+        else{ // se non lo è lo rimandi al login immediatamente
+            return "redirect:/login";
+        }
     }
 }
